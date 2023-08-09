93b098af9199e2693eb8f6e6284598d529c90cfe
Luís Soares <lsoares@newstore.com>
2023-08-09 23:48:23 +0100

    promotions

--- src/test/kotlin/FruitsTest.kt
+++ src/test/kotlin/FruitsTest.kt
@@ -25,13 +25,35 @@ class FruitsTest {
     }
 
     @Test
-    fun `multiple of each`() {
-        assertEquals(190, priceOf(List(2) { "🍏" } + List(2) { "🍊" }))
+    fun `2 apples pays 1`() {
+        assertEquals(25, priceOf(List(2) { "🍏" }))
+    }
+
+    @Test
+    fun `3 apples pays 2`() {
+        assertEquals(50, priceOf(List(3) { "🍏" }))
+    }
+
+    @Test
+    fun `3 oranges pays 2`() {
+        assertEquals(70 * 2, priceOf(List(3) { "🍊" }))
+    }
+
+    @Test
+    fun `4 oranges pays 3`() {
+        assertEquals(70 * 3, priceOf(List(4) { "🍊" }))
+    }
+
+    @Test
+    fun `apples and oranges`() {
+        assertEquals(25 + 140, priceOf(List(2) { "🍏" } + List(2) { "🍊" }))
     }
 }
 
 fun priceOf(fruits: List<String>): Int {
     val counts = fruits.groupBy { it }.mapValues { (_, occurrences) -> occurrences.size }
-    return counts.getOrDefault("🍏", 0) * 25 +
-            counts.getOrDefault("🍊", 0) * 70
+    val appleCount = counts.getOrDefault("🍏", 0)
+    val orangeCount = counts.getOrDefault("🍊", 0)
+    return (appleCount / 2 + appleCount % 2) * 25 +
+            ((orangeCount / 3) * 2 + orangeCount % 3) * 70
 }
