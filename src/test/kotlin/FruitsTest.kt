eed78869f2bb8dad6b4b0de82ab7bddbd3138a2b
Luís Soares <lsoares@newstore.com>
2023-08-12 16:33:02 +0100

    improve API

--- src/test/kotlin/FruitsTest.kt
+++ src/test/kotlin/FruitsTest.kt
@@ -5,68 +5,68 @@ class FruitsTest {
 
     @Test
     fun `price of empty cart`() {
-        assertEquals(0, priceOf(emptyList()))
+        assertEquals(0, priceOf())
     }
 
     @Test
     fun `1 apple`() {
-        assertEquals(25, priceOf(listOf("🍏")))
+        assertEquals(25, priceOf("🍏"))
     }
 
     @Test
     fun `1 orange`() {
-        assertEquals(70, priceOf(listOf("🍊")))
+        assertEquals(70, priceOf("🍊"))
     }
 
 
     @Test
     fun `1 orange and 1 apple`() {
-        assertEquals(95, priceOf(listOf("🍊", "🍏")))
+        assertEquals(95, priceOf("🍊", "🍏"))
     }
 
     @Test
     fun `2 apples pays 1`() {
-        assertEquals(25, priceOf(List(2) { "🍏" }))
+        assertEquals(25, priceOf("🍏", "🍏"))
     }
 
     @Test
     fun `3 apples pays 2`() {
-        assertEquals(50, priceOf(List(3) { "🍏" }))
+        assertEquals(50, priceOf("🍏", "🍏", "🍏"))
     }
 
     @Test
     fun `3 oranges pays 2`() {
-        assertEquals(70 * 2, priceOf(List(3) { "🍊" }))
+        assertEquals(70 * 2, priceOf("🍊", "🍊", "🍊"))
     }
 
     @Test
     fun `4 oranges pays 3`() {
-        assertEquals(70 * 3, priceOf(List(4) { "🍊" }))
+        assertEquals(70 * 3, priceOf(*Array(4) { "🍊" }))
     }
 
     @Test
     fun `apples and oranges`() {
-        assertEquals(25 + 140, priceOf(List(2) { "🍏" } + List(2) { "🍊" }))
+        assertEquals(25 + 140, priceOf(*Array(2) { "🍏" } + Array(2) { "🍊" }))
     }
 
     @Test
     fun `1 banana`() {
-        assertEquals(35, priceOf(listOf("🍌")))
+        assertEquals(35, priceOf("🍌"))
     }
 
     @Test
     fun `10 bananas pays 5`() {
-        assertEquals(35 * 5, priceOf(List(10) { "🍌" }))
+        assertEquals(35 * 5, priceOf(*Array(10) { "🍌" }))
     }
 
     @Test
     fun `2 bananas pays 1`() {
-        assertEquals(35, priceOf(List(2) { "🍌" }))
+        assertEquals(35, priceOf("🍌", "🍌"))
     }
 }
 
 
-fun priceOf(fruits: List<String>): Int {
+fun priceOf(vararg fruits: String): Int {
     val counts = fruits.groupBy { it }.mapValues { (_, occurrences) -> occurrences.size }
     val appleCount = counts.getOrDefault("🍏", 0)
     val orangeCount = counts.getOrDefault("🍊", 0)
